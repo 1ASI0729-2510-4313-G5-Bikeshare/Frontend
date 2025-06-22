959fa42f54bdd835af79e791f5f8c0985bc725ea
ALAYA1803 <rodrigoalaya_2004@hotmail.com>
2025-06-22 10:23:43 -0500

    fix: leaf component

--- src/app/renter/pages/map/map.page.ts
+++ src/app/renter/pages/map/map.page.ts
@@ -3,7 +3,7 @@ import { CommonModule, DecimalPipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 import * as L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
+
 
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
